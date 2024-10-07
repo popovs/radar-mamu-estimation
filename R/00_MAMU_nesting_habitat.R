05364428df98538635a93fa43452a43b8aa917a1
Sarah Popov <sarah.popov@gmail.com>
2024-10-06 22:52:18 -0700

    Create initial targets pipeline for DEM download and creation

---
+++ R/00_MAMU_nesting_habitat.R
@@ -0,0 +1,75 @@
+#' 0. MAMU NESTING HABITAT AREA
+
+#' This script contains functions that will download the BC 
+#' Digital Elevation Model (DEM) for our study region, then 
+#' clip it down to the areas that are highly likely to contain 
+#' suitable marbled murrelet nesting habitat area (regions 
+#' that are 30 km from shore and under <1500 m elevation). 
+#' The exact maximum elevation cutoff varies by our six 
+#' conservation regions of interest (North,  Central, and South 
+#' Mainland Coast, Haida Gwaii, North + West Vancouver Island, 
+#' and East Vancouver Island).
+
+
+# PREPARE GIS FILES -------------------------------------------------------
+
+prepare_regions <- function(filepath = "GIS/regions.gpkg") {
+  regions <- sf::st_read(filepath)
+  regions$region <- factor(regions$region, 
+                           levels = c("AKB", "HG", "NC", "CC", "SC", "NVI", "MWVI", "SWVI", "EVI"))
+  return(regions)
+}
+
+prepare_nests <- function(filepath = "GIS/MAMU_nests.gpkg", 
+                          uncertainty_col = "LOC_UNCE_1",
+                          regions = regions) {
+  nests <- sf::st_read(filepath)
+  nests <- sf::st_intersection(nests, regions)
+  # Add buffers of uncertainty around the nests
+  if (any(is.na(nests[[uncertainty_col]]))) stop("Nest locations must have an uncertainty associated with them (", uncertainty_col, ").")
+  message("Assuming nest uncertainty is in meters.")
+  #stopifnot("Nest location uncertainly calculations currently only support uncertainty expressed in meters (LOC_UNCE_2 column)." = all(tolower(unique(nests$LOC_UNCE_2)) %in% c("m", "meters", "meter")))
+  nests <- st_buffer(nests, dist = nests[[uncertainty_col]])
+  return(nests)
+}
+
+
+
+# PREPARE DEM -------------------------------------------------------------
+
+
+download_dem_tile <- function(tile,
+                              output_dir,
+                              save_output = TRUE,
+                              overwrite = TRUE) {
+  # Create output dir
+  output_dir <- file.path(output_dir, "DEM_tiles")
+  dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
+  # Download the tile
+  MAMU::BC_DEM(letterblock = tile, 
+               save_output = save_output,
+               overwrite = overwrite,
+               output_dir = output_dir)
+  out <- list.files(output_dir, full.names = TRUE)
+  out <- out[grep(paste0(tile, ".dem$"), out)]
+  return(out)
+}
+
+merge_dem <- function(vrt_path,
+                      output_file,
+                      overwrite = FALSE) {
+  # Re-project VRT
+  vrt <- terra::rast(vrt_path)
+  dem_3005 <- terra::project(vrt, "EPSG:3005")
+  # Save it
+  terra::writeRaster(dem_3005,
+                     filename = output_file, 
+                     overwrite = overwrite)
+  return(output_file)
+}
+
+resample_dem <- function(dem_path, res) {
+  dem <- terra::rast(dem_path)
+  # CHECK resolution of dem - only run the 'resample' bit
+  # if the res != resolution of dem
+}
--- _targets.R
+++ _targets.R
@@ -5,7 +5,7 @@
 
 # Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed.
+library(tarchetypes) # Needed for `tar_map()`
 
 # DISCLAIMER 1: THIS WHOLE SCRIPT WILL TAKE SEVERAL HOURS TO RUN.
 # There are likely faster or more efficient ways of doing this,
@@ -25,14 +25,62 @@ res <- 250 # res MUST be >25, as the DEM goes does to 25m accuracy.
 # Set target options:
 tar_option_set(
   packages = c("MAMU", # remotes::install_github("popovs/MAMU")
-               ),
-  format = "qs", 
+               "sf",
+               "terra"),
+  format = tar_format( # Default qs is superceded by qs2. Install qs2 and specify read & write fxns
+    read = function(path) { qs2::qs_read(path) }, 
+    write = function(object, path) { qs2::qs_save(object = object, file = path) }
+    ), 
   memory = "transient", # unload memory for each target line after it's completed
   garbage_collection = TRUE, # run gc() prior to each target
 )
 
+# Run the R scripts in the R/ folder with your custom functions:
 tar_source()
 
+# Objects to reference in pipeline
+tiles_to_download <- data.frame(tiles = c("103k", "103j", "103f", "103g", "103c", "103b", "102o", # haida gwaii
+                                          "103o", "103p", "103j", "103i", "103g", "103h", "103a", "93e", "93d", "93c", "93l", "93m", "102p", "92m", "92n", "104a", "104b", # north/central coast
+                                          "102i", "92l", "92k", "92e", "92f", "92c", "92b", # vancouver island
+                                          "92j", "92g", "92h" # lower mainland
+                                          ))
+DEM_dir <- "GIS/DEM"
+
+# Run tar_make() to execute the pipeline
 list(
-  
+  tar_target(regions, prepare_regions()),
+  tar_target(nests, prepare_nests(regions = regions)),
+  # PREPARE DEM 
+  # All the DEM data products will be saved as raster files on the
+  # disk rather than simply as _targets objects, so that QGIS can
+  # access and plot them. The targets pipeline will track changes
+  # to the files. 
+  # Download DEM tiles
+  tar_map(
+    values = tiles_to_download, # params need to be passed as a df/tibble, defined OUTSIDE the pipeline
+    # Download DEM tiles
+    tar_target(DEM_tiles,
+               download_dem_tile(tile = tiles, # `tiles` in this case refers to the `tiles` column in `tiles_to_download` df
+                                 save_output = TRUE,
+                                 overwrite = TRUE,
+                                 output_dir = DEM_dir),
+               format = "file")
+    ),
+  # Track files within the DEM_dir
+  tar_target(DEM_tile_files, list.files(file.path(DEM_dir, "DEM_tiles"), full.names = TRUE)),
+  # Make VRT
+  tar_target(DEM_VRT, 
+             terra::vrt(x = DEM_tile_files,
+                        filename = file.path(DEM_dir, "BC_DEM_VRT.vrt"),
+                        overwrite = TRUE,
+                        return_filename = TRUE), # for format "file" targets, the output MUST be a filepath
+             format = "file"),
+  # Merge DEM tiles and reproject to 3005 (~30 mins)
+  tar_target(BC_DEM_3005, 
+             merge_dem(vrt_path = DEM_VRT,
+                       output_file = file.path(DEM_dir, "BC_DEM_EPSG3005.tiff"),
+                       overwrite = TRUE),
+             format = "file")#,
+  # Resample to pipeline resolution (but no need to save it as its own tiff file)
+  #tar_target(DEM, [resample func here])
 )
