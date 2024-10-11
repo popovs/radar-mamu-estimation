2349d51be15d6ba2bb34ff89e1eca1a805eb548e
Sarah Popov <sarah.popov@gmail.com>
2024-10-10 17:15:04 -0700

    Fix stn fxn

---
+++ R/01-generate-radar-cones.R
@@ -0,0 +1,136 @@
+#' 01 GENERATE RADAR CONES
+
+#' This script contains functions that will extract
+#' radar station points, extract and calculate mean
+#' bird flight headings, and combine that information
+#' with watershed data and nest cost distance to
+#' create MAMU population catchments. That is, if
+#' a MAMU flies into a given watershed mouth, it is
+#' assumed it is targeting a nest within the corresponding
+#' catchment delineation.
+
+
+# PREPARE SURVEY DATA -----------------------------------------------------
+
+prepare_surveys <- function(path) {
+  s <- MAMU::process_radar_data(path)
+  s$site <- s$new_name # `process_radar_data()` stores cleaned up/consolidated site names in the `new_name` col
+  # Create spatial object
+  s <- s[!is.na(s$lon), ]
+  s <- sf::st_as_sf(s, coords = c("lon", "lat"), crs = 4326, remove = FALSE)
+  return(s)
+}
+
+# Extract individual stations from survey data
+# bc station lat/lon can change by a few meters each year
+# (impossible to set up the radar station in EXACTLY the
+# same spot each year), take the mean lat/lon by
+# site.
+prepare_stn <- function(s) {
+  stn <- sf::st_drop_geometry(s) # trying to get mean sf coords by group is a pain. 
+  stn <- stn |> 
+    dplyr::select(site, loc, lat, lon) |>
+    dplyr::group_by(site, loc) |>
+    dplyr::summarise(lat = mean(lat),
+                     lon = mean(lon)) |>
+    sf::st_as_sf(crs = 4326, 
+                 coords = c("lon", "lat"), 
+                 remove = FALSE)
+  return(stn)
+}
+
+
+prepare_headings <- function(path) {
+  headings <- readxl::read_excel(path,
+                                 na = c("", "NA", "#N/A", "N/A"),
+                                 col_types = "text")
+  headings <- janitor::clean_names(headings)
+  headings <- headings[!is.na(headings$heading),]
+  headings$lat <- as.numeric(headings$lat)
+  headings$lon <- as.numeric(headings$lon)
+  headings$speed <- as.numeric(headings$speed)
+  headings$distance <- as.numeric(headings$distance)
+  headings$heading <- as.numeric(headings$heading)
+  
+  # Flag if there's any likely data errors
+  stopifnot("You have headings >360 that are likely typos." = !any(headings$heading > 360))
+  
+  # Clean up station names as needed
+  stn_lookup <- MAMU::rs # rename station lookup table
+  headings <- merge(headings, stn_lookup, by.x = "name", by.y = "original_name", all.x = TRUE)
+  headings$name <- ifelse(is.na(headings$new_name), headings$name, headings$new_name)
+  
+  # TODO: Cut out stations with <5 headings?
+  
+  # Split out incoming and outgoing headings
+  inc <- headings[grep("Incoming", headings$flightpath_type, ignore.case = T),c("name", "heading")]
+  out <- headings[grep("Outgoing", headings$flightpath_type, ignore.case = T),c("name", "heading")]
+  
+  inc <- inc[complete.cases(inc),]
+  out <- out[complete.cases(out),]
+  
+  # Make `out` the opposite heading
+  # That is, we assume that 180° (polar opposite) direction
+  # of the outgoing headings is equivalent to incoming ones
+  out$heading <- out$heading - 180
+  out$heading <- ifelse(out$heading < 0, out$heading + 360, out$heading)
+  
+  # Now merge in and out back together
+  # `h` now functionally contains *'incoming' headings only*
+  h <- rbind(inc, out)
+  
+  return(h)
+}
+
+# Define circular mean function
+# where x is your heading angle IN RADIANS
+circmean <- function(x) { 
+  sinr <- sum(sin(x))
+  cosr <- sum(cos(x))
+  circmean <- atan2(sinr, cosr)
+  circmean
+}
+
+# Define circular bootstrapping function
+# For a given list `x` of radian measurements,
+# estimate the circular mean on a subsample of x.
+# Repeat `n` number of times to derive the bootstrapped
+# sample; derive quantile breaks `alpha` from the 
+# bootstrapped sample and output the result.
+# Thanks to Ben Bolker (again): https://stackoverflow.com/a/53916042/1454785
+circboot <- function(x, n, alpha) {
+  bootsample <- replicate(n, circmean(sample(x, replace = TRUE)))
+  out <- setNames(c(mean(bootsample), quantile(bootsample, 
+                                               c(alpha/2, 1-alpha/2))), 
+                  c("mean", "lower", "upper"))
+  return(out)
+}
+
+calc_polar_mean <- function(headings, n_reps, alpha) {
+  # For each station, take the POLAR MEAN INCOMING & OUTGOING heading.
+  # We need to use POLAR/CIRCULAR stats to deal with 'wraparound' angles -
+  # e.g. the mean of 45° and 315° is ~due north, but the cartesian mean 
+  # of the two -- 180° -- is directly due south!
+  
+  # First convert degrees to radians
+  headings$rad <- headings$heading * pi / 180
+  
+  # Calculate bootstrapped means and confidence intervals
+  # for each station
+  message("Calculating bootstrapped headings and 95 CI for bird headings at each station...")
+  h <- aggregate(rad ~ name, headings, FUN = circboot, n = n_reps, alpha = alpha)
+  message("Done bootstrapping.")
+  
+  # Tidy it up...
+  h <- cbind(h[1], data.frame(h$rad))
+  
+  # Calculate cone width
+  h$theta <- h$upper - h$lower
+  
+  # Convert radians back to degrees
+  h <- h |>
+    dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ .x * 180 / pi)) |>
+    dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ ifelse(. < 0, . + 360, .)))
+  
+  return(h)
+}
