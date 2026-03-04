ee857fb3d117e012b871171a88939751390b337f
popovs <sarah.popov@gmail.com>
2026-03-04 15:24:47 -0800

    Move prepare_surveys() to input_prep_fxns.R

--- R/input_prep_fxns.R
+++ R/input_prep_fxns.R
@@ -24,3 +24,23 @@ prepare_nests <- function(filepath, regions = regions) {
     unique()
   return(nests)
 }
+
+
+# PREPARE SURVEYS ---------------------------------------------------------
+
+prepare_surveys <- function(filepath, regions = regions) {
+  s <- MAMU::process_radar_data(filepath)
+  s$site <- s$new_name # `process_radar_data()` stores cleaned up/consolidated site names in the `new_name` col
+  # Create spatial object
+  s <- s[!is.na(s$lon), ]
+  s <- sf::st_as_sf(s, coords = c("lon", "lat"), crs = 4326, remove = FALSE)
+  # Merge in regions from pipeline
+  # Anything already stored in the pre-existing `region` col
+  # will be moved to `region_old`
+  s$region_old <- s$region
+  s <- dplyr::select(s, -region)
+  regions <- sf::st_transform(regions, 4326)
+  regions <- sf::st_make_valid(regions)
+  s <- sf::st_intersection(s, regions)
+  return(s)
+}
